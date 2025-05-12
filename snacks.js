69c10ba4361be802113f63029a343db80d640c5b
davidetoscano129 <toscano.davidew@gmail.com>
2025-05-12 20:17:49 +0200

    Snack 1 completed

---
+++ snacks.js
@@ -0,0 +1,7 @@
+// Snack 1
+function getInitials(nomeCompleto) {
+  const [nome, cognome] = nomeCompleto.split(" ").filter((str) => str !== " ");
+  return `${nome.charAt(0).toUpperCase()}.${cognome.charAt(0).toUpperCase()}.`;
+}
+
+module.exports = { getInitials };
